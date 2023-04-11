54196465be2ba935e4cda0d109ff6f9bd54af0f7
MichelAD1 <michel.a.abidaoud@gmail.com>
2023-04-11 22:45:07 +0300

    Creating design for the navbar

--- Front-end/urban-boutique-hotel/src/App.js
+++ Front-end/urban-boutique-hotel/src/App.js
@@ -1,26 +1,46 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ReactModal from "react-modal";
+import React, { useState } from "react";
 
 // Components
 import Login from "./Components/Auth/Login";
 import ForgotPassword from "./Components/Auth/ForgotPassword";
 import Home from "./Components/Home/Home";
+import Rooms from "./Components/Rooms/Rooms";
 import Navbar from "./Components/Navbar/Navbar";
+import PrivateRoute from "./Routes/PrivateRoutes";
 
 ReactModal.setAppElement("#root");
 function App() {
-	return (
-		<BrowserRouter>
-			<div className='App'>
-				<Navbar />
-				<Routes>
-					<Route path='/login' element={<Login />} />
-					<Route path='/reset' element={<ForgotPassword />} />
-					<Route path='/' element={<Home />} />
-				</Routes>
-			</div>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/reset" element={<ForgotPassword />} />
+
+          <Route
+            path="/"
+            element={
+              <>
+                <Navbar />
+                <Home />
+              </>
+            }
+          />
+          <Route
+            path="/rooms"
+            element={
+              <>
+                <Navbar />
+                <Rooms />
+              </>
+            }
+          />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- Front-end/urban-boutique-hotel/src/Components/Navbar/Navbar.js
+++ Front-end/urban-boutique-hotel/src/Components/Navbar/Navbar.js
@@ -1,31 +1,54 @@
-import { Link } from "react-router-dom";
-
-import Logo from "../../assets/images/logo.svg";
-import { FaAlignRight } from "react-icons/fa";
+import { useLocation, Link, useNavigate } from "react-router-dom";
+import { HiUserCircle } from "react-icons/hi";
 
 const Navbar = () => {
-	return (
-		<nav className='navbar'>
-			<div className='nav-center'>
-				<div className='nav-header'>
-					<Link to='/'>
-						<img src={Logo} alt='Hotel Booking' />
-					</Link>
-					<button type='button' className='nav-btn'>
-						<FaAlignRight className='nav-icon' />
-					</button>
-				</div>
-				<ul className='nav-links show-nav'>
-					<li>
-						<Link to='/'>Home</Link>
-					</li>
-					<li>
-						<Link to='/rooms'>Rooms</Link>
-					</li>
-				</ul>
-			</div>
-		</nav>
-	);
+  const path = useLocation().pathname;
+  const navigate = useNavigate();
+  return (
+    <nav className="navbar">
+      <div className="links-section">
+        {" "}
+        <Link to="/" className="nav-link">
+          <div className={`routes ${path === "/" ? "active" : ""}`}>Home</div>
+        </Link>
+        <Link to="/rooms" className="nav-link">
+          <div className={`routes ${path === "/rooms" ? "active" : ""}`}>
+            Rooms
+          </div>
+        </Link>
+        <Link to="/offers" className="nav-link">
+          <div className={`routes ${path === "/offers" ? "active" : ""}`}>
+            Services
+          </div>
+        </Link>
+        <Link to="/findus" className="nav-link">
+          <div className={`routes ${path === "/findus" ? "active" : ""}`}>
+            Find Us
+          </div>
+        </Link>
+        <Link to="/discover" className="nav-link">
+          <div className={`routes ${path === "/discover" ? "active" : ""}`}>
+            About
+          </div>
+        </Link>
+        <Link to="/contact" className="nav-link">
+          <div className={`routes ${path === "/contact" ? "active" : ""}`}>
+            Contact
+          </div>
+        </Link>
+      </div>
+      <div className="login-section">
+        <div className="login-btn">
+          <Link to="/login" className="nav-link">
+            <div className="login-img">
+              <HiUserCircle className="nav-image" />
+            </div>
+            <div className="routes nav-btn">Sign In</div>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;

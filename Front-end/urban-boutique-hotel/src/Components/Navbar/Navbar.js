e1bac1d133b3977ed7e42b19b2d37457e40832bf
Marc Issa <issamarc15@gmail.com>
2023-04-13 00:43:55 +0300

    feat(Navbar.js): add close icon to navbar
    feat(Navbar.js): add open and close functionality to navbar and login button
    
    style(index.css): adjust layout and add styles to navbar and login button
    feat(index.css): add support for responsive design in navbar and login button

--- Front-end/urban-boutique-hotel/src/Components/Navbar/Navbar.js
+++ Front-end/urban-boutique-hotel/src/Components/Navbar/Navbar.js
@@ -1,6 +1,7 @@
 import { useLocation, Link } from "react-router-dom";
 import { HiUserCircle } from "react-icons/hi";
 import { FaAlignRight } from "react-icons/fa";
+import { AiOutlineClose } from "react-icons/ai";
 import { useState } from "react";
 
 const Navbar = () => {
@@ -10,55 +11,79 @@ const Navbar = () => {
 
 	const handleToggle = () => {
 		setActive(!active);
+		const navbar = document.getElementById("navbar");
+		const login_btn = document.getElementById("login-btn");
+
+		navbar.style.height = "auto";
+		login_btn.style.display = "flex";
+	};
+
+	const handleClose = () => {
+		setActive(!active);
+		const navbar = document.getElementById("navbar");
+		const login_btn = document.getElementById("login-btn");
+
+		navbar.style.height = "4em";
+		login_btn.style.display = "none";
 	};
 
 	return (
-		<nav className='navbar'>
+		<nav className='navbar' id='navbar'>
 			<div className='nav-image'>Logo</div>
-			<ul className={active ? "links-section show-nav" : "links-section"}>
+			<ul className={active ? "links-section" : "links-section show-nav"}>
 				{" "}
-				<Link to='/' className='nav-link'>
+				<Link to='/' className='nav-link' onClick={handleClose}>
 					<li className={`routes ${path === "/" ? "active" : ""}`}>Home</li>
 				</Link>
-				<Link to='/rooms' className='nav-link'>
+				<Link to='/rooms' className='nav-link' onClick={handleClose}>
 					<li className={`routes ${path === "/rooms" ? "active" : ""}`}>
 						Rooms
 					</li>
 				</Link>
-				<Link to='/services' className='nav-link'>
+				<Link to='/services' className='nav-link' onClick={handleClose}>
 					<li className={`routes ${path === "/services" ? "active" : ""}`}>
 						Services
 					</li>
 				</Link>
-				<Link to='/findus' className='nav-link'>
+				<Link to='/findus' className='nav-link' onClick={handleClose}>
 					<li className={`routes ${path === "/findus" ? "active" : ""}`}>
 						Find Us
 					</li>
 				</Link>
-				<Link to='/about' className='nav-link'>
+				<Link to='/about' className='nav-link' onClick={handleClose}>
 					<li className={`routes ${path === "/about" ? "active" : ""}`}>
 						About
 					</li>
 				</Link>
-				<Link to='/contact' className='nav-link'>
+				<Link to='/contact' className='nav-link' onClick={handleClose}>
 					<li className={`routes ${path === "/contact" ? "active" : ""}`}>
 						Contact
 					</li>
 				</Link>
 			</ul>
 			<div className='login-section'>
-				<div className='login-btn'>
-					<Link to='/login' className='nav-link'>
+				<div className='login-btn' id='login-btn'>
+					<Link to='/login' className='nav-link' onClick={handleClose}>
 						<div className='login-img'>
 							<HiUserCircle className='nav-image' />
 						</div>
-						<div className='routes'>Sign In</div>
+						<div className='routes'>Log in</div>
 					</Link>
 				</div>
 			</div>
-			<button type='button' className='nav-btn' onClick={handleToggle}>
-				<FaAlignRight className='nav-icon' />
-			</button>
+			{!active && (
+				<button type='button' className='nav-btn' onClick={handleToggle}>
+					<FaAlignRight className='nav-icon' />
+				</button>
+			)}
+			{active && (
+				<button
+					type='button'
+					className='nav-btn close-btn'
+					onClick={handleClose}>
+					<AiOutlineClose className='nav-icon' />
+				</button>
+			)}
 		</nav>
 	);
 };

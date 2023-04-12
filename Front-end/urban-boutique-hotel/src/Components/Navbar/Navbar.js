34d8465aa21de48406b42fd64f6b06e269647e42
Marc Issa <issamarc15@gmail.com>
2023-04-12 23:54:45 +0300

    feat(Navbar.js): add responsive menu toggle button and refactor navbar links to unordered list
    style(index.css): add list-style-type none and display block to .nav-link
    style(index.css): add margin-right to .nav-image
    style(index.css): change font-size and background-color of .nav-btn
    style(index.css): add .nav-icon class
    style(index.css): add media query for larger screens and change .nav-btn opacity and cursor

--- Front-end/urban-boutique-hotel/src/Components/Navbar/Navbar.js
+++ Front-end/urban-boutique-hotel/src/Components/Navbar/Navbar.js
@@ -1,54 +1,66 @@
-import { useLocation, Link, useNavigate } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
 import { HiUserCircle } from "react-icons/hi";
+import { FaAlignRight } from "react-icons/fa";
+import { useState } from "react";
 
 const Navbar = () => {
-  const path = useLocation().pathname;
-  const navigate = useNavigate();
-  return (
-    <nav className="navbar">
-      <div className="links-section">
-        {" "}
-        <Link to="/" className="nav-link">
-          <div className={`routes ${path === "/" ? "active" : ""}`}>Home</div>
-        </Link>
-        <Link to="/rooms" className="nav-link">
-          <div className={`routes ${path === "/rooms" ? "active" : ""}`}>
-            Rooms
-          </div>
-        </Link>
-        <Link to="/services" className="nav-link">
-          <div className={`routes ${path === "/services" ? "active" : ""}`}>
-            Services
-          </div>
-        </Link>
-        <Link to="/findus" className="nav-link">
-          <div className={`routes ${path === "/findus" ? "active" : ""}`}>
-            Find Us
-          </div>
-        </Link>
-        <Link to="/about" className="nav-link">
-          <div className={`routes ${path === "/about" ? "active" : ""}`}>
-            About
-          </div>
-        </Link>
-        <Link to="/contact" className="nav-link">
-          <div className={`routes ${path === "/contact" ? "active" : ""}`}>
-            Contact
-          </div>
-        </Link>
-      </div>
-      <div className="login-section">
-        <div className="login-btn">
-          <Link to="/login" className="nav-link">
-            <div className="login-img">
-              <HiUserCircle className="nav-image" />
-            </div>
-            <div className="routes nav-btn">Sign In</div>
-          </Link>
-        </div>
-      </div>
-    </nav>
-  );
+	const path = useLocation().pathname;
+
+	const [active, setActive] = useState(false);
+
+	const handleToggle = () => {
+		setActive(!active);
+	};
+
+	return (
+		<nav className='navbar'>
+			<div className='nav-image'>Logo</div>
+			<ul className={active ? "links-section show-nav" : "links-section"}>
+				{" "}
+				<Link to='/' className='nav-link'>
+					<li className={`routes ${path === "/" ? "active" : ""}`}>Home</li>
+				</Link>
+				<Link to='/rooms' className='nav-link'>
+					<li className={`routes ${path === "/rooms" ? "active" : ""}`}>
+						Rooms
+					</li>
+				</Link>
+				<Link to='/services' className='nav-link'>
+					<li className={`routes ${path === "/services" ? "active" : ""}`}>
+						Services
+					</li>
+				</Link>
+				<Link to='/findus' className='nav-link'>
+					<li className={`routes ${path === "/findus" ? "active" : ""}`}>
+						Find Us
+					</li>
+				</Link>
+				<Link to='/about' className='nav-link'>
+					<li className={`routes ${path === "/about" ? "active" : ""}`}>
+						About
+					</li>
+				</Link>
+				<Link to='/contact' className='nav-link'>
+					<li className={`routes ${path === "/contact" ? "active" : ""}`}>
+						Contact
+					</li>
+				</Link>
+			</ul>
+			<div className='login-section'>
+				<div className='login-btn'>
+					<Link to='/login' className='nav-link'>
+						<div className='login-img'>
+							<HiUserCircle className='nav-image' />
+						</div>
+						<div className='routes'>Sign In</div>
+					</Link>
+				</div>
+			</div>
+			<button type='button' className='nav-btn' onClick={handleToggle}>
+				<FaAlignRight className='nav-icon' />
+			</button>
+		</nav>
+	);
 };
 
 export default Navbar;

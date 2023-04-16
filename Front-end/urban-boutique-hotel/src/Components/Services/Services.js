f84432984f8e1a84b896cfca8174a170b9ecf864
MichelAD1 <michel.a.abidaoud@gmail.com>
2023-04-17 00:27:46 +0300

    Adding image for the second section

--- Front-end/urban-boutique-hotel/src/Components/Services/Services.js
+++ Front-end/urban-boutique-hotel/src/Components/Services/Services.js
@@ -25,26 +25,26 @@ const Services = () => {
           <div className="service-content">
             <h3 className="service-title">BREAKFAST</h3>
             <p className="service-description">
-              Start the day with a rich breakfast. Freshly prepared daily, we
-              offer our guests a well-balanced meal. From 07:30 to 10:00 we
-              serve a buffet in our breakfast room. If you want a "breakfast to
-              go" instead of breakfast, our staff can also prepare you for it.
+              Begin your day with a delicious and nutritious breakfast, freshly
+              prepared daily. We serve a buffet in our breakfast room from 07:30
+              to 10:00. If you prefer a "breakfast to go", our staff will gladly
+              prepare it for you.
             </p>
           </div>
         </div>
         <div className="service-item">
           <div className="service-content">
-            <h3 className="service-title">BREAKFAST</h3>
+            <h3 className="service-title">GROUP OFFERS</h3>
             <p className="service-description">
-              Start the day with a rich breakfast. Freshly prepared daily, we
-              offer our guests a well-balanced meal. From 07:30 to 10:00 we
-              serve a buffet in our breakfast room. If you want a "breakfast to
-              go" instead of breakfast, our staff can also prepare you for it.
+              Whether you're traveling with a school group, a professional
+              delegation, or for a wedding, we have customized group packages to
+              fit your needs. Contact us to learn more about our tailored offers
+              for groups.
             </p>
           </div>
           <div className="service-image-container-inversed">
             <img
-              src={require("../../assets/images/breakfast.jpg")}
+              src={require("../../assets/images/group-offers.jpg")}
               alt="image"
               className="service-image"
             />

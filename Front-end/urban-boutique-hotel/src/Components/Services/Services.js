e230b574c5c10de976f6aeff130187ce864abaf0
MichelAD1 <michel.a.abidaoud@gmail.com>
2023-04-16 22:52:08 +0300

    Creating components for services

--- Front-end/urban-boutique-hotel/src/Components/Services/Services.js
+++ Front-end/urban-boutique-hotel/src/Components/Services/Services.js
@@ -13,6 +13,24 @@ const Services = () => {
           </Link>
         </div>
       </div>
+      <div className="service-item">
+        <div className="service-image-container">
+          <img
+            src={require("../../assets/images/breakfast.jpg")}
+            alt="image"
+            className="service-image"
+          />
+        </div>
+        <div className="service-content">
+          <h3 className="service-title">BREAKFAST</h3>
+          <p className="service-description">
+            Start the day with a rich breakfast. Freshly prepared daily, we
+            offer our guests a well-balanced meal. From 07:30 to 10:00 we serve
+            a buffet in our breakfast room. If you want a "breakfast to go"
+            instead of breakfast, our staff can also prepare you for it.
+          </p>
+        </div>
+      </div>
     </>
   );
 };

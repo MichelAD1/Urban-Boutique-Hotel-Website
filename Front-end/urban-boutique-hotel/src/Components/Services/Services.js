7899bd76bcff520f321a2b5e02877fd1cedebb14
MichelAD1 <michel.a.abidaoud@gmail.com>
2023-04-16 22:29:01 +0300

    Creating services welcome design

--- Front-end/urban-boutique-hotel/src/Components/Services/Services.js
+++ Front-end/urban-boutique-hotel/src/Components/Services/Services.js
@@ -1,7 +1,20 @@
-import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
-function Services() {
-  return;
-}
+const Services = () => {
+  return (
+    <>
+      <div className="servicesHero">
+        <div className="banner">
+          <h1>Services</h1>
+          <div></div>
+          <Link to="/services" className="btn-primary">
+            Discover our exclusive packages
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Services;

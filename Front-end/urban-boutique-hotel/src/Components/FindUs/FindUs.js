c2661510111278885881d5cfc7ef31d8fa6b4f7b
Marc Issa <issamarc15@gmail.com>
2023-04-18 18:20:06 +0300

    feat(FindUs.js): add Slideshow component and images for Schönbrunn Palace and The Hofburg
    feat(Slideshow.js): add Slideshow component to display a slideshow of images
    feat(Slideshow.js): add automatic slideshow functionality with a delay of 2500ms
    feat(Slideshow.js): add dot indicators to show current slide and allow manual navigation
    feat(Slideshow.js): receive data prop to display images in the slideshow
    feat(Slideshow.js): add resetTimeout function to clear timeout when component unmounts
    feat(Slideshow.js): use useEffect hook to update index state and reset timeout on index change
    feat(Slideshow.js): use useState hook to manage index state
    feat(Slideshow.js): use useRef hook to store timeout reference
    feat(Slideshow.js): add slide and slideshowSlider classes to style slideshow
    feat(Slideshow.js): add slideshowDots and slideshowDot classes to style dot indicators
    feat(Slideshow.js): add key prop to slide component to avoid warning
    feat(Slideshow.js): add inline
    
    feat(images): add new images to assets folder
    
    style(index.css): update hero images and add slideshow styles

--- Front-end/urban-boutique-hotel/src/Components/FindUs/FindUs.js
+++ Front-end/urban-boutique-hotel/src/Components/FindUs/FindUs.js
@@ -1,7 +1,27 @@
-import React, { useState } from "react";
+import Slideshow from "../../Global/Components/Slideshow";
+
+// Images
+import att_1 from "../../assets/images/att-1.jpg";
+import att_2 from "../../assets/images/att-2.jpg";
 
 function FindUs() {
-  return;
+	const data = [
+		{
+			id: 1,
+			title: "Schönbrunn Palace",
+			description:
+				"Schönbrunn Palace is a Baroque palace in Vienna, Austria built as a summer residence for the Habsburgs. It has over 1,400 rooms, luxurious furnishings, and intricate decorations showcasing Habsburg opulence. The palace gardens feature lawns, flower beds, fountains, and landmarks such as the Neptune Fountain and Gloriette. It is a popular tourist attraction and a UNESCO World Heritage Site.",
+			image: att_1,
+		},
+		{
+			id: 2,
+			title: "The Hofburg",
+			description:
+				"The Hofburg is a large palace complex in the center of Vienna, Austria. It has served as the residence of the Habsburg dynasty and the Austrian government for over 700 years. The palace features impressive Baroque and Gothic architecture and is home to several museums, including the Imperial Apartments, the Sisi Museum, and the Austrian National Library. Today, it is one of Vienna's most popular tourist attractions, offering a glimpse into Austria's rich history and culture.",
+			image: att_2,
+		},
+	];
+	return <Slideshow data={data} />;
 }
 
 export default FindUs;
---
+++ Front-end/urban-boutique-hotel/src/Global/Components/Slideshow.js
@@ -0,0 +1,54 @@
+import { useEffect, useState, useRef } from "react";
+
+const Slideshow = ({ data }) => {
+	const delay = 2500;
+
+	const [index, setIndex] = useState(0);
+	const timeoutRef = useRef(null);
+
+	function resetTimeout() {
+		if (timeoutRef.current) {
+			clearTimeout(timeoutRef.current);
+		}
+	}
+
+	useEffect(() => {
+		resetTimeout();
+		timeoutRef.current = setTimeout(
+			() =>
+				setIndex((prevIndex) =>
+					prevIndex === data.length - 1 ? 0 : prevIndex + 1,
+				),
+			delay,
+		);
+
+		return () => {
+			resetTimeout();
+		};
+	}, [index]);
+
+	return (
+		<div className='slideshow'>
+			<div
+				className='slideshowSlider'
+				style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}>
+				{data.map((backgroundColor, index) => (
+					<div className='slide' key={index} style={{ backgroundColor }}></div>
+				))}
+			</div>
+
+			<div className='slideshowDots'>
+				{data.map((_, idx) => (
+					<div
+						key={idx}
+						className={`slideshowDot${index === idx ? " active" : ""}`}
+						onClick={() => {
+							setIndex(idx);
+						}}></div>
+				))}
+			</div>
+		</div>
+	);
+};
+
+export default Slideshow;

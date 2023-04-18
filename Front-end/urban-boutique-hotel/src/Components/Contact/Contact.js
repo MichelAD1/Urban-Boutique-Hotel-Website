4b5f055a712e6dd9ee21d62fa5397ef1fdd3a0c9
MichelAD1 <michel.a.abidaoud@gmail.com>
2023-04-18 17:19:57 +0300

    Creating contact banner and adding footer for the contact page

--- Front-end/urban-boutique-hotel/src/Components/Contact/Contact.js
+++ Front-end/urban-boutique-hotel/src/Components/Contact/Contact.js
@@ -1,7 +1,29 @@
-import React, { useState } from "react";
+import { Link as ScrollLink } from "react-scroll";
+import { useState, useEffect } from "react";
+import Footer from "../../Global/Components/Footer";
 
-function Contact() {
-  return;
-}
+const Contact = () => {
+  return (
+    <>
+      <div className="contactHero">
+        <div className="banner">
+          <h1>Contact</h1>
+          <div></div>
+          <ScrollLink
+            to="contact-section"
+            smooth={true}
+            duration={1000}
+            offset={-100}
+            className="btn-primary"
+          >
+            Contact us to book
+          </ScrollLink>
+        </div>
+      </div>
+
+      <Footer />
+    </>
+  );
+};
 
 export default Contact;

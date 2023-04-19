9eec89bb939c64d6d225485cbcee074101c4aa13
Marc Issa <issamarc15@gmail.com>
2023-04-19 20:43:55 +0300

    Creating the design for the settings componenets

--- Front-end/urban-boutique-hotel/src/Components/Profile/Profile.js
+++ Front-end/urban-boutique-hotel/src/Components/Profile/Profile.js
@@ -1,5 +1,7 @@
-// Components
-import Setting from "./Setting";
+import { Link } from "react-router-dom";
+
+// Icons
+import { MdManageAccounts } from "react-icons/md";
 
 const Profile = () => {
 	return (
@@ -7,8 +9,37 @@ const Profile = () => {
 			<div className='profile-section'>
 				<h2>Account settings</h2>
 				<h5>Manage your booking experience</h5>
-				<div className='settings-section'>
-					<Setting />
+				<div className='settings-container'>
+					<div className='settings-box'>
+						<MdManageAccounts className='settings-icon' />
+						<div className='settings-content'>
+							<h3>Personal information</h3>
+							<p>Update your personal information</p>
+							<Link to='' className='settings-link'>
+								Manage your personal details
+							</Link>
+						</div>
+					</div>
+					<div className='settings-box'>
+						<MdManageAccounts className='settings-icon' />
+						<div className='settings-content'>
+							<h3>Personal information</h3>
+							<p>Update your personal information</p>
+							<Link to='' className='settings-link'>
+								Manage your personal details
+							</Link>
+						</div>
+					</div>
+					<div className='settings-box'>
+						<MdManageAccounts className='settings-icon' />
+						<div className='settings-content'>
+							<h3>Personal information</h3>
+							<p>Update your personal information</p>
+							<Link to='' className='settings-link'>
+								Manage your personal details
+							</Link>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>

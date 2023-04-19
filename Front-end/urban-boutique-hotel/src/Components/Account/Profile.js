6100d009bb2199718be4ea3dccd4ed176c228351
Marc Issa <issamarc15@gmail.com>
2023-04-19 22:53:41 +0300

    feat(Profile.js): add user profile information and edit button functionality
    style(index.css): add styles for profile-btn and info-item classes
    style(index.css): adjust padding for account-item class
    style(index.css): adjust width for info-item class

--- Front-end/urban-boutique-hotel/src/Components/Account/Profile.js
+++ Front-end/urban-boutique-hotel/src/Components/Account/Profile.js
@@ -1,21 +1,123 @@
-import { IoIosArrowBack } from "react-icons/io";
+import { useState, useEffect } from "react";
+
+// Components
+import Footer from "../../Global/Components/Footer";
 const Profile = () => {
+	const [user, setUser] = useState({});
+	useEffect(() => {
+		setUser({
+			id: 1,
+			name: "John Doe",
+			username: "john",
+			email: "johndoe@example.com",
+			phone: "+961 71 578 965",
+			dob: "21/05/2000",
+			nationality: "Lebanese",
+			address: "Beirut, Lebanon",
+			gender: "Male",
+		});
+	}, []);
+
+	const handleEdit = () => {
+		console.log("Edit");
+	};
 	return (
-		<div className='profile-container'>
-			<div className='profile-section'>
-				<div className='profile-item'>
-					<h2>Personal information</h2>
-					<h5>Update your personal information</h5>
-				</div>
-				<div className='account-item'>
-					<div className='account-info'>
-						<label>Name</label>
-						<p>Marc Issa</p>
+		<>
+			<div className='profile-container'>
+				<div className='profile-section'>
+					<div className='profile-item'>
+						<div className='profile-title'>
+							<h2>Personal information</h2>
+							<h5>Update your personal information</h5>
+						</div>
+						<button type='button' className='profile-btn' onClick={handleEdit}>
+							Edit
+						</button>
 					</div>
-					<button type='button'>Edit</button>
+					<div className='account-item'>
+						<div className='account-info'>
+							<div className='info-item'>
+								<label>Name</label>
+							</div>
+							<div className='info-item'>
+								<p>{user.name}</p>
+							</div>
+						</div>
+					</div>
+					<div className='account-item'>
+						<div className='account-info'>
+							<div className='info-item'>
+								<label>Display name</label>
+							</div>
+							<div className='info-item'>
+								<p>{user.username}</p>
+							</div>
+						</div>
+					</div>
+					<div className='account-item'>
+						<div className='account-info'>
+							<div className='info-item'>
+								<label>Email</label>
+							</div>
+							<div className='info-item'>
+								<p>{user.email}</p>
+							</div>
+						</div>
+					</div>
+					<div className='account-item'>
+						<div className='account-info'>
+							<div className='info-item'>
+								<label>Phone number</label>
+							</div>
+							<div className='info-item'>
+								<p>{user.phone}</p>
+							</div>
+						</div>
+					</div>
+					<div className='account-item'>
+						<div className='account-info'>
+							<div className='info-item'>
+								<label>Date of birth</label>
+							</div>
+							<div className='info-item'>
+								<p>{user.dob}</p>
+							</div>
+						</div>
+					</div>
+					<div className='account-item'>
+						<div className='account-info'>
+							<div className='info-item'>
+								<label>Nationality</label>
+							</div>
+							<div className='info-item'>
+								<p>{user.nationality}</p>
+							</div>
+						</div>
+					</div>
+					<div className='account-item'>
+						<div className='account-info'>
+							<div className='info-item'>
+								<label>Gender</label>
+							</div>
+							<div className='info-item'>
+								<p>{user.gender}</p>
+							</div>
+						</div>
+					</div>
+					<div className='account-item'>
+						<div className='account-info'>
+							<div className='info-item'>
+								<label>Address</label>
+							</div>
+							<div className='info-item'>
+								<p>{user.address}</p>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
-		</div>
+			<Footer />
+		</>
 	);
 };
 

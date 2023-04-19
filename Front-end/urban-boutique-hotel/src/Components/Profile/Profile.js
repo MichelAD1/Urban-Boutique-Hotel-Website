bd8c0b0cc2df2aff12ab10adbb89f09e720e3755
Marc Issa <issamarc15@gmail.com>
2023-04-19 20:14:08 +0300

    feat(Profile.js): add account settings section to profile page feat(Setting.js): add Setting component style(index.css): add styles for profile section and settings section

--- Front-end/urban-boutique-hotel/src/Components/Profile/Profile.js
+++ Front-end/urban-boutique-hotel/src/Components/Profile/Profile.js
@@ -1,5 +1,18 @@
+// Components
+import Setting from "./Setting";
+
 const Profile = () => {
-	return <div>Profile page</div>;
+	return (
+		<div className='profile-container'>
+			<div className='profile-section'>
+				<h2>Account settings</h2>
+				<h5>Manage your booking experience</h5>
+				<div className='settings-section'>
+					<Setting />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Profile;

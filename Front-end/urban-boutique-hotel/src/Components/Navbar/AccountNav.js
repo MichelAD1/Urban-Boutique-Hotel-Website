80a248c4107085e66a9bc5e816c82f2310396e19
Marc Issa <issamarc15@gmail.com>
2023-04-19 21:34:05 +0300

    style(AccountNav.js): remove empty divs from the navbar style(index.css): add media query for account image on smaller screens and hide settings link on smaller screens

--- Front-end/urban-boutique-hotel/src/Components/Navbar/AccountNav.js
+++ Front-end/urban-boutique-hotel/src/Components/Navbar/AccountNav.js
@@ -5,10 +5,8 @@ import { HiUserCircle } from "react-icons/hi";
 const AccountNav = () => {
 	return (
 		<nav className='account-navbar' id='navbar'>
-			<div></div>
 			<img className='nav-image' src={logo} />
 			<HiUserCircle className='account-image' />
-			<div></div>
 		</nav>
 	);
 };

603b71f5f05243f66d39936dcc3a83c7e2e13444
MichelAD1 <michel.a.abidaoud@gmail.com>
2023-04-19 19:36:03 +0300

    Creating and making routes for booking page

--- Front-end/urban-boutique-hotel/src/App.js
+++ Front-end/urban-boutique-hotel/src/App.js
@@ -8,6 +8,7 @@ import ForgotPassword from "./Components/Auth/ForgotPassword";
 import Home from "./Components/Home/Home";
 import Rooms from "./Components/Rooms/Rooms";
 import RoomsItem from "./Components/Rooms/RoomsItem";
+import Book from "./Components/Rooms/Book";
 import Services from "./Components/Services/Services";
 import FindUs from "./Components/FindUs/FindUs";
 import Discover from "./Components/Discover/Discover";
@@ -19,86 +20,95 @@ import ScrollToTop from "./Global/Function/ScrollToTop";
 
 ReactModal.setAppElement("#root");
 function App() {
-	return (
-		<BrowserRouter>
-			<div className='App'>
-				<Routes>
-					<Route path='/login' element={<Login />} />
-					<Route path='/reset' element={<ForgotPassword />} />
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/reset" element={<ForgotPassword />} />
 
-					<Route
-						path='/'
-						element={
-							<>
-								<ScrollToTop />
-								<Navbar />
-								<Home />
-							</>
-						}
-					/>
-					<Route
-						path='/rooms'
-						element={
-							<>
-								<ScrollToTop />
-								<Navbar />
-								<Rooms />
-							</>
-						}
-					/>
-					<Route
-						path='/rooms/:id'
-						element={
-							<>
-								<ScrollToTop />
-								<RoomsItem />
-							</>
-						}
-					/>
-					<Route
-						path='/services'
-						element={
-							<>
-								<ScrollToTop />
-								<Navbar />
-								<Services />
-							</>
-						}
-					/>
-					<Route
-						path='/findus'
-						element={
-							<>
-								<ScrollToTop />
-								<Navbar />
-								<FindUs />
-							</>
-						}
-					/>
-					<Route
-						path='/discover'
-						element={
-							<>
-								<ScrollToTop />
-								<Navbar />
-								<Discover />
-							</>
-						}
-					/>
-					<Route
-						path='/contact'
-						element={
-							<>
-								<ScrollToTop />
-								<Navbar />
-								<Contact />
-							</>
-						}
-					/>
-				</Routes>
-			</div>
-		</BrowserRouter>
-	);
+          <Route
+            path="/"
+            element={
+              <>
+                <ScrollToTop />
+                <Navbar />
+                <Home />
+              </>
+            }
+          />
+          <Route
+            path="/rooms"
+            element={
+              <>
+                <ScrollToTop />
+                <Navbar />
+                <Rooms />
+              </>
+            }
+          />
+          <Route
+            path="/rooms/:roomname"
+            element={
+              <>
+                <ScrollToTop />
+                <RoomsItem />
+              </>
+            }
+          />
+          <Route
+            path="/rooms/booking"
+            element={
+              <>
+                <ScrollToTop />
+                <Book />
+              </>
+            }
+          />
+          <Route
+            path="/services"
+            element={
+              <>
+                <ScrollToTop />
+                <Navbar />
+                <Services />
+              </>
+            }
+          />
+          <Route
+            path="/findus"
+            element={
+              <>
+                <ScrollToTop />
+                <Navbar />
+                <FindUs />
+              </>
+            }
+          />
+          <Route
+            path="/discover"
+            element={
+              <>
+                <ScrollToTop />
+                <Navbar />
+                <Discover />
+              </>
+            }
+          />
+          <Route
+            path="/contact"
+            element={
+              <>
+                <ScrollToTop />
+                <Navbar />
+                <Contact />
+              </>
+            }
+          />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ Front-end/urban-boutique-hotel/src/Components/Rooms/Book.js
@@ -0,0 +1,8 @@
+import { useLocation, useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+const Book = () => {
+  return;
+};
+
+export default Book;

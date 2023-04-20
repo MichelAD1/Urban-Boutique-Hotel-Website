56bd80561d2c65b24c4aae5b9f7e0b9bef2b13f7
Marc Issa <issamarc15@gmail.com>
2023-04-20 19:27:37 +0300

    Changing the navbar icons

--- Front-end/admin-web-portal/src/Componenents/NavBar/Navbar.js
+++ Front-end/admin-web-portal/src/Componenents/NavBar/Navbar.js
@@ -4,115 +4,111 @@ import { useLocation, Link, useNavigate } from "react-router-dom";
 // Logo
 import logo from "../../assets/logo.png";
 
-// Icons no fill
-import home_nf from "../../assets/icons/home-nf.svg";
-import options_nf from "../../assets/icons/apps-nf.svg";
-import room_nf from "../../assets/icons/room-nf.svg";
-import employee_nf from "../../assets/icons/employees-nf.svg";
-import user_nf from "../../assets/icons/person-nf.svg";
-import logout_nf from "../../assets/icons/logout-nf.svg";
-
-// Icons fill
-import home_fill from "../../assets/icons/home-fill.svg";
-import room_fill from "../../assets/icons/room-fill.svg";
-import employee_fill from "../../assets/icons/employee-fill.svg";
-import user_fill from "../../assets/icons/person-fill.svg";
-// import logout_fill from "../../assets/icons/logout-nf.svg";
+// Icons
+import { AiOutlineHome, AiFillHome } from "react-icons/ai";
+import {
+	IoOptionsOutline,
+	IoOptions,
+	IoBedOutline,
+	IoBed,
+} from "react-icons/io5";
+import { HiOutlineUserGroup, HiUserGroup } from "react-icons/hi";
+import { BsPerson, BsPersonFill } from "react-icons/bs";
+import { MdLogout } from "react-icons/md";
 
 import Logout from "../../api-client/Auth/Logout";
 
 export default function Navbar() {
-  const path = useLocation().pathname;
-  const navigate = useNavigate();
+	const path = useLocation().pathname;
+	const navigate = useNavigate();
 
-  return (
-    <nav className="navbar">
-      <div>
-        <div className="logo">
-          <img src={logo} alt="" />
-        </div>
-        {/* Home Page navigation */}
-        {path !== "/" && (
-          <Link to="/" className="nav-link">
-            <img className="icons" src={home_nf} alt="" />
-            <div className="routes">Home</div>
-          </Link>
-        )}
-        {path === "/" && (
-          <div className="nav-link">
-            <img className="icons" src={home_fill} alt="" />
-            <div className="routes active">Home</div>
-          </div>
-        )}
-        {/* Options Page navigation */}
-        {path === "/options" && (
-          <div className="nav-link">
-            <img className="icons" src={options_nf} alt="" />
-            <div className="routes active">Options</div>
-          </div>
-        )}
+	return (
+		<nav className='navbar'>
+			<div>
+				<div className='logo'>
+					<img src={logo} alt='' />
+				</div>
+				{/* Home Page navigation */}
+				{path !== "/" && (
+					<Link to='/' className='nav-link'>
+						<AiOutlineHome className='icons' alt='home' />
+						<div className='routes'>Home</div>
+					</Link>
+				)}
+				{path === "/" && (
+					<div className='nav-link'>
+						<AiFillHome className='icons' alt='' />
+						<div className='routes active'>Home</div>
+					</div>
+				)}
+				{/* Options Page navigation */}
+				{path === "/options" && (
+					<div className='nav-link'>
+						<IoOptions className='icons' alt='' />
+						<div className='routes active'>Options</div>
+					</div>
+				)}
 
-        {path !== "/options" && (
-          <Link to="/options" className="nav-link">
-            <img className="icons" src={options_nf} alt="" />
-            <div className="routes">Options</div>
-          </Link>
-        )}
-        {/* Room Page navigation */}
-        {path !== "/rooms" && (
-          <Link to="/rooms" className="nav-link">
-            <img className="icons" src={room_nf} alt="" />
-            <div className="routes">Rooms</div>
-          </Link>
-        )}
-        {path === "/rooms" && (
-          <div className="nav-link">
-            <img className="icons" src={room_fill} alt="" />
-            <div className="routes active">Rooms</div>
-          </div>
-        )}
-        {/* Employees Page navigation */}
-        {path !== "/employees" && (
-          <Link to="/employees" className="nav-link">
-            <img className="icons" src={employee_nf} alt="" />
-            <div className="routes">Staff</div>
-          </Link>
-        )}
-        {path === "/employees" && (
-          <div className="nav-link">
-            <img className="icons" src={employee_fill} alt="" />
-            <div className="routes active">Staff</div>
-          </div>
-        )}
-        {/* Users Page navigation */}
-        {path === "/users" && (
-          <div className="nav-link">
-            <img className="icons" src={user_fill} alt="" />
-            <div className="routes active">Users</div>
-          </div>
-        )}
-        {path !== "/users" && (
-          <Link to="/users" className="nav-link">
-            <img className="icons" src={user_nf} alt="" />
-            <div className="routes">Users</div>
-          </Link>
-        )}
-      </div>
+				{path !== "/options" && (
+					<Link to='/options' className='nav-link'>
+						<IoOptionsOutline className='icons' alt='' />
+						<div className='routes'>Options</div>
+					</Link>
+				)}
+				{/* Room Page navigation */}
+				{path !== "/rooms" && (
+					<Link to='/rooms' className='nav-link'>
+						<IoBedOutline className='icons' alt='' />
+						<div className='routes'>Rooms</div>
+					</Link>
+				)}
+				{path === "/rooms" && (
+					<div className='nav-link'>
+						<IoBed className='icons' alt='' />
+						<div className='routes active'>Rooms</div>
+					</div>
+				)}
+				{/* Employees Page navigation */}
+				{path !== "/employees" && (
+					<Link to='/employees' className='nav-link'>
+						<HiOutlineUserGroup className='icons' alt='' />
+						<div className='routes'>Staff</div>
+					</Link>
+				)}
+				{path === "/employees" && (
+					<div className='nav-link'>
+						<HiUserGroup className='icons' alt='' />
+						<div className='routes active'>Staff</div>
+					</div>
+				)}
+				{/* Users Page navigation */}
+				{path === "/users" && (
+					<div className='nav-link'>
+						<BsPersonFill className='icons' alt='' />
+						<div className='routes active'>Users</div>
+					</div>
+				)}
+				{path !== "/users" && (
+					<Link to='/users' className='nav-link'>
+						<BsPerson className='icons' alt='' />
+						<div className='routes'>Users</div>
+					</Link>
+				)}
+			</div>
 
-      <div
-        className="nav-link logout"
-        onClick={() => {
-          const response = Logout();
-          response
-            .then((res) => {
-              navigate("/login");
-            })
-            .catch((err) => err);
-        }}
-      >
-        <img className="icons" src={logout_nf} alt="" />
-        <div className="routes">Log out</div>
-      </div>
-    </nav>
-  );
+			<div
+				className='nav-link logout'
+				onClick={() => {
+					const response = Logout();
+					response
+						.then((res) => {
+							navigate("/login");
+						})
+						.catch((err) => err);
+				}}>
+				<MdLogout className='icons' alt='' />
+				<div className='routes'>Log out</div>
+			</div>
+		</nav>
+	);
 }

fbb8330aa17bb22a1bd24ce77a71c6f1d85e8546
Marc Issa <issamarc15@gmail.com>
2023-04-21 21:05:37 +0300

    Fixing routing to maintenance requests

--- Front-end/admin-web-portal/src/App.js
+++ Front-end/admin-web-portal/src/App.js
@@ -17,7 +17,7 @@ import Finance from "./Componenents/Finance/Finance";
 import Rooms from "./Componenents/Rooms/Rooms";
 import RoomItem from "./Componenents/Rooms/RoomItem";
 import MaintenanceRequest from "./Global/Components/Maintenance Request/Requests";
-import RequestItem from "./Global/Components/Businesses Request/RequestItem";
+import RequestItem from "./Global/Components/Maintenance Request/RequestItem";
 import PrivateRoute from "./Routes/PrivateRoutes";
 
 ReactModal.setAppElement("#root");

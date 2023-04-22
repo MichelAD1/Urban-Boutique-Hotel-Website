d64a333c7a8b0d353c8f09d4cf4cc38607bd858f
Marc Issa <issamarc15@gmail.com>
2023-04-23 00:14:54 +0300

    feat(ReviewItem.js): add functionality to display and edit review data in admin portal
    feat(Rating.js): add Rating component to display star rating based on rate prop
    style(index.css): add color to .review-rating class to match secondary color scheme

--- Front-end/admin-web-portal/src/Componenents/Support/Reviews/ReviewItem.js
+++ Front-end/admin-web-portal/src/Componenents/Support/Reviews/ReviewItem.js
@@ -1,5 +1,79 @@
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+// Components
+import Rating from "../../../Global/Components/Rating";
+
 const ReviewItem = () => {
-	return <div>Review Item</div>;
+	const loc = useLocation();
+	const [data, setData] = useState(loc.state.data);
+
+	const [username, setUsername] = useState(data.username);
+	const [review, setReview] = useState(data.review);
+	const [rating, setRating] = useState(data.rating);
+	const [date, setDate] = useState(data.date);
+
+	useEffect(() => {
+		setUsername(data.username);
+		setReview(data.review);
+		setRating(data.rating);
+		setDate(data.date);
+	}, []);
+
+	function capitalizeFirstLetter(string) {
+		return string.charAt(0).toUpperCase() + string.slice(1);
+	}
+
+	return (
+		<div className='container'>
+			<div className='edit-container'>
+				<div className='edit-item'>
+					<h2>Review #{data.id}</h2>
+					<button className='button'>Ban</button>
+				</div>
+				<div className='edit-item'>
+					<div className='edit-info'>
+						<div>
+							<label>Username</label>
+						</div>
+						<div>
+							<p>{username}</p>
+						</div>
+					</div>
+				</div>
+				<div className='edit-item'>
+					<div className='edit-info'>
+						<div>
+							<label>Review</label>
+						</div>
+						<div>
+							<p>{review}</p>
+						</div>
+					</div>
+				</div>
+				<div className='edit-item'>
+					<div className='edit-info'>
+						<div>
+							<label>Rating</label>
+						</div>
+						<div>
+							<Rating rate={rating} />
+						</div>
+					</div>
+				</div>
+				<div className='edit-item'>
+					<div className='edit-info'>
+						<div>
+							<label>Date</label>
+						</div>
+						<div>
+							<p>{date}</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default ReviewItem;

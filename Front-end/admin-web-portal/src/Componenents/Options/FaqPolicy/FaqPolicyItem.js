5e0263a4083358ce2b31eaaa844eba438e2d34a9
Marc Issa <issamarc15@gmail.com>
2023-04-23 20:06:04 +0300

    feat(FaqPolicyItem.js): add commented out code for a modal and edit-container to be used later
    refactor(OptionsCard.js): replace Link component with onClick event and useNavigate hook for navigation
    feat(OptionsCard.js): add redirect function to navigate to /options/faqs_policies/info route with data state

--- Front-end/admin-web-portal/src/Componenents/Options/FaqPolicy/FaqPolicyItem.js
+++ Front-end/admin-web-portal/src/Componenents/Options/FaqPolicy/FaqPolicyItem.js
@@ -1,5 +1,104 @@
+import { useState, useEffect } from "react";
+
 const FaqPolicyItem = () => {
-	return <div>Faq Policy Item</div>;
+	return (
+		<div className='container'>
+			{/* <div className='edit-container'>
+				<div className='edit-item'>
+					<h2>Review #{data.id}</h2>
+					{!replied && (
+						<button className='save-button' onClick={handleAddReply}>
+							Save
+						</button>
+					)}
+					<button className='button' onClick={handleAdd}>
+						Show
+					</button>
+				</div>
+				<div className='edit-item'>
+					<div className='edit-info info-large'>
+						<div>
+							<label>Username</label>
+						</div>
+						<div>
+							<p>{username}</p>
+						</div>
+					</div>
+				</div>
+				<div className='edit-item'>
+					<div className='edit-info info-large'>
+						<div>
+							<label>Review</label>
+						</div>
+						<div>
+							<p>{review}</p>
+						</div>
+					</div>
+				</div>
+				<div className='edit-item'>
+					<div className='edit-info info-large'>
+						<div>
+							<label>Rating</label>
+						</div>
+						<div>
+							<Rating rate={rating} />
+						</div>
+					</div>
+				</div>
+				<div className='edit-item'>
+					<div className='edit-info info-large'>
+						<div>
+							<label>Date</label>
+						</div>
+						<div>
+							<p>{date}</p>
+						</div>
+					</div>
+				</div>
+				<div className='edit-item'>
+					<div className='edit-info info-large'>
+						<div style={{ alignSelf: "flex-start" }}>
+							<label>Reply</label>
+						</div>
+						<div>
+							{replied && <p>{reply}</p>}
+							{!replied && (
+								<textarea
+									value={reply}
+									onChange={(e) => setReply(e.target.value)}
+									className='input-box bio-input'
+								/>
+							)}
+						</div>
+					</div>
+				</div>
+			</div>
+			<ReactModal
+				className='custom-modal'
+				isOpen={isModalOpen}
+				style={{
+					overlay: { backgroundColor: "rgba(0, 0, 0, 0.5)" },
+					content: {
+						backgroundColor: "rgba(0, 0, 0, 0.5)",
+						border: "none",
+						width: "100%",
+						height: "100%",
+						margin: "auto",
+						display: "flex",
+						justifyContent: "center",
+						alignItems: "center",
+						zIndex: "100",
+					},
+				}}>
+				<div>
+					<h1>Confirm Add</h1>
+					<p>Are you sure you want to add this to the website home page?</p>
+					<button onClick={handleConfirmAdd}>Yes</button>
+					<button onClick={closeModal}>No</button>
+				</div>
+			</ReactModal> */}
+		</div>
+	);
 };
 
 export default FaqPolicyItem;
--- Front-end/admin-web-portal/src/Global/Components/OptionsCard.js
+++ Front-end/admin-web-portal/src/Global/Components/OptionsCard.js
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
 
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const OptionsCard = (reqData) => {
 	const [data, setData] = useState([]);
+
+	const navigate = useNavigate();
 
 	useEffect(() => {
 		setData(reqData.reqData);
@@ -15,14 +17,18 @@ const OptionsCard = (reqData) => {
 		}
 	};
 
+	const redirect = () => {
+		navigate("/options/faqs_policies/info", { state: { data: data } });
+	};
+
 	return (
-		<Link to='/options/faqs_policies/info' className='options-card'>
+		<div className='options-card' onClick={redirect}>
 			<div className='options-card-header'>
 				<h2>{data.title}</h2>
 				<span>{capitalize(data.tag)}</span>
 			</div>
 			<p>{data.description}</p>
-		</Link>
+		</div>
 	);
 };
 

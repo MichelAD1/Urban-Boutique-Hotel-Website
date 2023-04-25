e9087bac2c788206a042ba592685b203190298bc
MichelAD1 <michel.a.abidaoud@gmail.com>
2023-04-26 00:46:28 +0300

    Finishing desgin for privacy policy

--- Front-end/urban-boutique-hotel/src/Components/Policies/Policy.js
+++ Front-end/urban-boutique-hotel/src/Components/Policies/Policy.js
@@ -1,5 +1,44 @@
 const Policy = () => {
-  return;
+  return (
+    <div className="policies-container">
+      <h1>urban boutique hotel privacy policy</h1>
+      <p>Welcome to Urban Boutique Hotel</p>
+      <p>Last Updated: March 14, 2023.</p>
+      <p>
+        Located in the heart of Vienna, Urban Boutique Hotel offers luxurious
+        accommodations and unparalleled service to our guests. Our boutique
+        hotel boasts a prime location for exploring the city's rich history and
+        culture, and is perfect for both leisure and business travelers. This
+        privacy policy outlines how we collect, use, and share information on or
+        in connection with our hotel services. If you have any questions or
+        concerns, please don't hesitate to contact us using the information
+        provided in the "Contact Us" section below. When we refer to "Urban
+        Boutique Hotel" (or any similar terms like "we" or "us") in this privacy
+        policy, we mean the entity that controls and is responsible for your
+        information. This includes all subsidiaries that help provide and
+        support our hotel services. Thank you for choosing Urban Boutique Hotel
+        as your destination in Vienna. We look forward to providing you with a
+        memorable and comfortable stay.
+      </p>
+      <h3 className="policies-title">1. what are the hotel services?</h3>
+      <p>
+        Located in the heart of Vienna, Urban Boutique Hotel offers luxurious
+        accommodations and unparalleled service to our guests. Our boutique
+        hotel boasts a prime location for exploring the city's rich history and
+        culture, and is perfect for both leisure and business travelers. This
+        privacy policy outlines how we collect, use, and share information on or
+        in connection with our hotel services. If you have any questions or
+        concerns, please don't hesitate to contact us using the information
+        provided in the "Contact Us" section below. When we refer to "Urban
+        Boutique Hotel" (or any similar terms like "we" or "us") in this privacy
+        policy, we mean the entity that controls and is responsible for your
+        information. This includes all subsidiaries that help provide and
+        support our hotel services. Thank you for choosing Urban Boutique Hotel
+        as your destination in Vienna. We look forward to providing you with a
+        memorable and comfortable stay.
+      </p>
+    </div>
+  );
 };
 
 export default Policy;

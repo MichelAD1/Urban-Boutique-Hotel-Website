adcdf6d1bc7549a7ed7158c172d8ad7c3bc22945
MichelAD1 <michel.a.abidaoud@gmail.com>
2023-04-26 00:55:03 +0300

    Finishing faq design

--- Front-end/urban-boutique-hotel/src/Components/Policies/FAQ.js
+++ Front-end/urban-boutique-hotel/src/Components/Policies/FAQ.js
@@ -1,5 +1,29 @@
 const FAQ = () => {
-  return;
+  return (
+    <div className="faq-container">
+      <h1>FREQUENTLY ASKED QUESTIONS</h1>
+      <div className="faq">
+        <p className="faq-question">
+          Which platforms does the Urban Boutique Hotel support?
+        </p>
+        <p className="faq-response">
+          The Hotel currently offers PC and Mac support. You can check platform
+          compatibility for individual titles by referring to the
+          "Specifications" section of any product page.
+        </p>
+      </div>
+      <div className="faq">
+        <p className="faq-question">
+          Which platforms does the Urban Boutique Hotel support?
+        </p>
+        <p className="faq-response">
+          The Hotel currently offers PC and Mac support. You can check platform
+          compatibility for individual titles by referring to the
+          "Specifications" section of any product page.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default FAQ;

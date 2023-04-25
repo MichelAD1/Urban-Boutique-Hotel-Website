8bc2844570c7a0f25e8a8b35db8b791fddafc44b
MichelAD1 <michel.a.abidaoud@gmail.com>
2023-04-26 01:35:56 +0300

    Fixing style for policy

--- Front-end/urban-boutique-hotel/src/Components/Policies/Policy.js
+++ Front-end/urban-boutique-hotel/src/Components/Policies/Policy.js
@@ -39,16 +39,21 @@ const Policy = () => {
             boutique hotel boasts a prime location for exploring the city's rich
             history and culture, and is perfect for both leisure and business
             travelers. This privacy policy outlines how we collect, use, and
-            share information on or in connection with our hotel services. If
-            you have any questions or concerns, please don't hesitate to contact
-            us using the information provided in the "Contact Us" section below.
-            When we refer to "Urban Boutique Hotel" (or any similar terms like
-            "we" or "us") in this privacy policy, we mean the entity that
-            controls and is responsible for your information. This includes all
-            subsidiaries that help provide and support our hotel services. Thank
-            you for choosing Urban Boutique Hotel as your destination in Vienna.
-            We look forward to providing you with a memorable and comfortable
-            stay.
+            share information on or in connection with our hotel services.
+          </p>
+          <p>
+            {" "}
+            If you have any questions or concerns, please don't hesitate to
+            contact us using the information provided in the "Contact Us"
+            section. When we refer to "Urban Boutique Hotel" (or any similar
+            terms like "we" or "us") in this privacy policy, we mean the entity
+            that controls and is responsible for your information. This includes
+            all subsidiaries that help provide and support our hotel services.
+          </p>
+          <p>
+            Thank you for choosing Urban Boutique Hotel as your destination in
+            Vienna. We look forward to providing you with a memorable and
+            comfortable stay.
           </p>
           {policies.map((policy) => (
             <div key={policy.id}>

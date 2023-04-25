25d9c9c54ea60d69b1f9470396ba250d5b4fb8fa
Michael Matar <mike.matar97@gmail.com>
2023-04-26 01:14:55 +0300

    fixing migrations

--- APIs/database/migrations/2023_04_25_220827_update_regulations_table.php
+++ APIs/database/migrations/2023_04_25_220827_update_regulations_table.php
@@ -11,7 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::table('regulations', function (Blueprint $table) {
+            $table->string('title')->after('text');
+        });
     }
 
     /**

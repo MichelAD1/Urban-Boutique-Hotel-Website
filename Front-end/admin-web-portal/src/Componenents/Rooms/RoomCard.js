224883b5c05152034b92e5ed960bb9d7498ddb47
Marc Issa <issamarc15@gmail.com>
2023-04-26 19:13:02 +0300

    refactor(admin-web-portal): remove react-redux dependency from package.json fix(RoomCard.js): change data.name to data.title and add sqft to size, add USD to price, calculate new price based on rent and discount, and remove unused image source in img tag
    feat(Rooms.js): add search icon to search bar
    refactor(Rooms.js): remove unused import of room1 image
    refactor(Rooms.js): use useQuery hook from @tanstack/react-query instead of useState and useEffect
    
    refactor(Rooms.js): remove hard-coded data and use useQuery hook to fetch rooms data from server
    
    refactor(Rooms.js): replace search icon image with AiOutlineSearch component
    refactor(Rooms.js): update Room component props to match new data structure
    
    feat(GetRoom.js): use base_url constant instead of hardcoded URL
    style(index.css): add font-size and color to search-icon class

--- Front-end/admin-web-portal/src/Componenents/Rooms/RoomCard.js
+++ Front-end/admin-web-portal/src/Componenents/Rooms/RoomCard.js
@@ -4,29 +4,29 @@ import { useNavigate } from "react-router-dom";
 
 function RoomCard({ data }) {
 	const navigate = useNavigate();
+	console.log(data);
 	const showPopup = (item) => {
 		navigate("/room/profile", { state: { data: item } });
 	};
-	console.log(data);
 	return (
 		<div className='room-card' onClick={() => showPopup(data)}>
 			<div className='room-logo'>
-				<img className='image' src={data.images[0]} alt='Room image' />
+				<img className='image' alt='Room image' />
 			</div>
-			<div className='name'>{data.name}</div>
+			<div className='name'>{data.title}</div>
 			<div className='room-details'>
 				<p className='paragraph'>
-					<b>Size:</b> {data.size}
+					<b>Size:</b> {data.size} sqft
 				</p>
 				<p className='paragraph'>
-					<b>Price:</b> {data.price}
+					<b>Price:</b> USD {data.rent}
 				</p>
-				{data.old_price && (
+				{data.discount !== 0 && (
 					<p className='paragraph'>
-						<b>New Price</b> {data.old_price}
+						<b>New Price</b> {data.rent - data.discount}
 					</p>
 				)}
-				{!data.old_price && (
+				{data.discount === 0 && (
 					<p className='paragraph'>
 						<br />
 					</p>

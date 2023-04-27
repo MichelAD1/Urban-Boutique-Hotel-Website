6d93f7f918a9b436b099c65fecf826781ac48b31
Marc Issa <issamarc15@gmail.com>
2023-04-27 23:27:11 +0300

    refactor(DeleteRoom.js): replace hardcoded URL with base_url constant variable.

--- Front-end/admin-web-portal/src/api-client/Rooms/DeleteRoom.js
+++ Front-end/admin-web-portal/src/api-client/Rooms/DeleteRoom.js
@@ -1,15 +1,16 @@
 import axios from "axios";
+import base_url from "../BaseUrl";
 
 export default async function DeleteRoom(roomid) {
-  const resp = await axios
-    .get(`http://localhost:8000/api/v0.1/room/delete/${roomid}`, {
-      headers: { Authorization: localStorage.getItem("token") },
-    })
-    .then((res) => {
-      return res;
-    })
-    .catch((err) => {
-      return err;
-    });
-  return resp;
+	const resp = await axios
+		.get(`${base_url}room/remove/${roomid}`, {
+			headers: { Authorization: localStorage.getItem("token") },
+		})
+		.then((res) => {
+			return res;
+		})
+		.catch((err) => {
+			return err;
+		});
+	return resp;
 }

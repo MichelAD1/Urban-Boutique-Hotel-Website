c6be1a2d0cfa4634a4d8c67048be2efa1a53adee
MichelAD1 <michel.a.abidaoud@gmail.com>
2023-04-30 00:31:31 +0300

    Creating front-end for reservation edit

--- Front-end/urban-boutique-hotel/src/Components/Account/Reservation.js
+++ Front-end/urban-boutique-hotel/src/Components/Account/Reservation.js
@@ -3,10 +3,37 @@ import { useState, useEffect } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { useTranslation } from "react-i18next";
+import Footer from "../../Global/Components/Footer";
+
 const Reservation = () => {
   const location = useLocation();
   const room = location.state.data;
-  return;
+  const { t, i18n } = useTranslation();
+
+  useEffect(() => {
+    i18n.changeLanguage(localStorage.getItem("Translate"));
+  }, []);
+  const handleSubmit = () => {};
+  return (
+    <>
+      <div className="reservations-edit-container">
+        <div className="reservations">
+          <h2>{t("acc_res")}</h2>
+        </div>
+        <form className="message-inputs book" onSubmit={handleSubmit}>
+          <div className="message-name-email">
+            <div className="message-input">
+              <DatePicker placeholderText={t("res_checkin")} />
+            </div>
+            <div className="message-input">
+              <DatePicker placeholderText={t("res_checkout")} />
+            </div>
+          </div>
+        </form>
+      </div>
+      <Footer />
+    </>
+  );
 };
 
 export default Reservation;

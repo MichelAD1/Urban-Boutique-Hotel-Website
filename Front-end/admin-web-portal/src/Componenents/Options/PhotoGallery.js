cd616c36e95a4d27c0745304c1515e6fbcf80db2
Marc Issa <issamarc15@gmail.com>
2023-04-30 01:39:33 +0300

    feat(PhotoGallery.js): add photo gallery component with support for adding and deleting images. Add useQuery hook to fetch images from server. Add state to track added and deleted images.
    refactor(PhotoGallery.js): rename addImage function to handleImageUpload and add handleImageDelete function to handle image deletion. Also, add error handling for image upload.
    
    feat(api): add GetImages function to retrieve images from the server

--- Front-end/admin-web-portal/src/Componenents/Options/PhotoGallery.js
+++ Front-end/admin-web-portal/src/Componenents/Options/PhotoGallery.js
@@ -1,35 +1,38 @@
 import { useState, useEffect } from "react";
+import { useQuery } from "@tanstack/react-query";
 
 // Icons
 import { AiOutlinePlus } from "react-icons/ai";
 import { RiDeleteBin2Fill } from "react-icons/ri";
 
-// Images
-import room1 from "../../assets/dummy.png";
-import room2 from "../../assets/dummy.png";
-import room3 from "../../assets/dummy.png";
+// Photo Gallery
+import GetImages from "../../api-client/Options/GetImages";
 
 const PhotoGallery = () => {
 	const [edit, setEdit] = useState(false);
 
 	const [images, setImages] = useState([]);
 
+	const [err, setErr] = useState("");
+
+	const [addedImages, setAddedImages] = useState([]);
+	const [deletedImages, setDeletedImages] = useState([]);
+
+	const [imagesChanged, setImagesChanged] = useState(false);
+
+	const {
+		status,
+		error,
+		data: photoGalleryData,
+	} = useQuery(["photo_gallery"], GetImages);
 	useEffect(() => {
-		setImages([
-			{
-				id: 1,
-				image: room1,
-			},
-			{
-				id: 2,
-				image: room2,
-			},
-			{
-				id: 3,
-				image: room3,
-			},
-		]);
-	}, []);
+		if (photoGalleryData) {
+			if (photoGalleryData.length === 0) {
+				setErr("No images found");
+			}
+			setImages(photoGalleryData);
+		}
+	}, [photoGalleryData, status]);
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
@@ -40,14 +43,38 @@ const PhotoGallery = () => {
 		setEdit(false);
 	};
 
-	const addImage = (e) => {
+	const convertImageToBase64 = (image, id) => {
+		return new Promise((resolve, reject) => {
+			const reader = new FileReader();
+			reader.readAsDataURL(image);
+			reader.onload = () => resolve({ id, image_url: reader.result });
+			reader.onerror = (error) => reject(error);
+		});
+	};
+
+	const handleImageUpload = async (e) => {
 		const files = e.target.files;
-		const tmp_images = [];
-		for (let i = 0; i < files.length; i++) {
-			tmp_images.push(files[i]);
+
+		const base64Images = await Promise.all(
+			Object.values(files).map((file, id) => {
+				return convertImageToBase64(file, images.length + id);
+			}),
+		);
+		setImages([...images, ...base64Images]);
+		const reqImages = [];
+		for (let i = 0; i < base64Images.length; i++) {
+			reqImages.push(base64Images[i].image_url);
 		}
-		// setImages(...images, tmp_images);
-		console.log(tmp_images);
+		setAddedImages([...addedImages, ...reqImages]);
+		setImagesChanged(true);
+		setErr("");
+	};
+
+	const handleImageDelete = (index) => {
+		const newImages = images.filter((image) => image.id !== index);
+		setImages(newImages);
+		setDeletedImages([...deletedImages, index]);
+		setImagesChanged(true);
 	};
 
 	return (
@@ -90,7 +117,7 @@ const PhotoGallery = () => {
 									<input
 										type='file'
 										multiple
-										onChange={(e) => addImage(e)}
+										onChange={(e) => handleImageUpload(e)}
 										id='images-upload'
 										name='images-upload'
 										className='upload-image'
@@ -99,14 +126,22 @@ const PhotoGallery = () => {
 							)}
 						</div>
 						<div className='gallery'>
-							{images.map((image) => {
-								return (
-									<div key={image.id}>
-										<img className='gallery-images' src={image.image} />
-										{edit && <RiDeleteBin2Fill className='delete-icon' />}
-									</div>
-								);
-							})}
+							{!err}
+							{!err &&
+								images.map((image) => {
+									return (
+										<div key={image.id}>
+											<img className='gallery-images' src={image.image_url} />
+											{edit && (
+												<RiDeleteBin2Fill
+													className='delete-icon'
+													onClick={() => handleImageDelete(image.id)}
+												/>
+											)}
+										</div>
+									);
+								})}
+							{err && <p>{err}</p>}
 						</div>
 					</div>
 				</div>

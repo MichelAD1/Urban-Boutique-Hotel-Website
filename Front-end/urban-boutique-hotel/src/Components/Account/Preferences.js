8a4a9d63984b59a495a8cf9fb11859f9998178fe
MichelAD1 <michel.a.abidaoud@gmail.com>
2023-05-01 01:03:31 +0300

    Creating load buffer for preferences page

--- Front-end/urban-boutique-hotel/src/Components/Account/Preferences.js
+++ Front-end/urban-boutique-hotel/src/Components/Account/Preferences.js
@@ -1,12 +1,32 @@
 import React, { useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
+import { useQuery } from "@tanstack/react-query";
+
+//apis
+import GetCurrency from "../../api-client/Account/GetCurrency";
 
 const Preferences = () => {
   const { t, i18n } = useTranslation();
 
   const [edit, setEdit] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [currency, setCurrency] = useState("USD");
   const [language, setLanguage] = useState(localStorage.getItem("Lg"));
+
+  const {
+    status,
+    error,
+    data: currencyData,
+  } = useQuery(["currencydata"], GetCurrency, {
+    staleTime: 300000, // 5 minutes
+  });
+
+  useEffect(() => {
+    if (status === "success" && currencyData) {
+      console.log(currencyData);
+      setLoading(false);
+    }
+  }, [currencyData, status, error]);
 
   useEffect(() => {
     handleCancel();
@@ -82,64 +102,91 @@ const Preferences = () => {
               </button>
             </div>
           </div>
-          <div className="account-item">
-            <div className="account-info">
-              <div className="info-item">
-                <label>{t("currency")}</label>
+          {loading ? (
+            <div>
+              <div className="account-item">
+                <div className="account-info">
+                  <div className="info-item">
+                    <label>{t("currency")}</label>
+                  </div>
+                  <div className="info-item">
+                    <div className="buffer-loader"></div>
+                  </div>
+                </div>
               </div>
-              {edit && (
-                <div className="info-item">
-                  <select
-                    className="account-input"
-                    value={currency}
-                    onChange={(e) => {
-                      setCurrency(e.target.value);
-                    }}
-                  >
-                    <option value="USD">USD</option>
-                    <option value="EUR">EUR</option>
-                    <option value="GBP">GBP</option>
-                    <option value="AUD">AUD</option>
-                    <option value="CAD">CAD</option>
-                  </select>
+              <div className="account-item">
+                <div className="account-info">
+                  <div className="info-item">
+                    <label>{t("language")}</label>
+                  </div>
+                  <div className="info-item">
+                    <div className="buffer-loader"></div>
+                  </div>
                 </div>
-              )}
-              {!edit && (
-                <div className="info-item">
-                  <p>{currency}</p>
-                </div>
-              )}
-            </div>
-          </div>
-          <div className="account-item">
-            <div className="account-info">
-              <div className="info-item">
-                <label>{t("language")}</label>
               </div>
-              {edit && (
-                <div className="info-item">
-                  <select
-                    className="account-input"
-                    value={language}
-                    onChange={(e) => {
-                      setLanguage(e.target.value);
-                    }}
-                  >
-                    <option value="English">English</option>
-                    <option value="Spanish">Spanish</option>
-                    <option value="French">French</option>
-                    <option value="German">German</option>
-                    <option value="Italian">Italian</option>
-                  </select>
-                </div>
-              )}
-              {!edit && (
-                <div className="info-item">
-                  <p>{language}</p>
-                </div>
-              )}
             </div>
-          </div>
+          ) : (
+            <div>
+              <div className="account-item">
+                <div className="account-info">
+                  <div className="info-item">
+                    <label>{t("currency")}</label>
+                  </div>
+                  {edit && (
+                    <div className="info-item">
+                      <select
+                        className="account-input"
+                        value={currency}
+                        onChange={(e) => {
+                          setCurrency(e.target.value);
+                        }}
+                      >
+                        <option value="USD">USD</option>
+                        <option value="EUR">EUR</option>
+                        <option value="GBP">GBP</option>
+                        <option value="AUD">AUD</option>
+                        <option value="CAD">CAD</option>
+                      </select>
+                    </div>
+                  )}
+                  {!edit && (
+                    <div className="info-item">
+                      <p>{currency}</p>
+                    </div>
+                  )}
+                </div>
+              </div>
+              <div className="account-item">
+                <div className="account-info">
+                  <div className="info-item">
+                    <label>{t("language")}</label>
+                  </div>
+                  {edit && (
+                    <div className="info-item">
+                      <select
+                        className="account-input"
+                        value={language}
+                        onChange={(e) => {
+                          setLanguage(e.target.value);
+                        }}
+                      >
+                        <option value="English">English</option>
+                        <option value="Spanish">Spanish</option>
+                        <option value="French">French</option>
+                        <option value="German">German</option>
+                        <option value="Italian">Italian</option>
+                      </select>
+                    </div>
+                  )}
+                  {!edit && (
+                    <div className="info-item">
+                      <p>{language}</p>
+                    </div>
+                  )}
+                </div>
+              </div>
+            </div>
+          )}
         </form>
       </div>
     </>

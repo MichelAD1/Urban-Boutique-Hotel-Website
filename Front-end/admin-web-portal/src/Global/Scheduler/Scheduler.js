c5562667950283e52cb5a9df20f6da7276828ad4
Marc Issa <issamarc15@gmail.com>
2023-05-01 09:16:17 +0300

    refactor(Scheduler.js): remove hard-coded appointments and add React Query for fetching and editing tasks feat(Scheduler.js): add useState and useEffect hooks for managing state and side effects
    refactor(Scheduler.js): refactor class component to functional component using hooks
    feat(Scheduler.js): add state variables for appointments, shadePreviousCells, shadePreviousAppointments, and updateInterval using useState hook
    feat(Scheduler.js): add useEffect hook to fetch tasks data and update appointments state variable
    feat(Scheduler.js): add commitChanges function to handle adding, editing, and deleting appointments
    feat(Scheduler.js): add Scheduler, ViewState, EditingState, WeekView, DayView, Appointments, IntegratedEditing, Toolbar, ViewSwitcher, DateNavigator, TodayButton, AppointmentTooltip, AppointmentForm, DragDropProvider, and CurrentTimeIndicator components to render the scheduler view
    
    feat(Scheduler): add EditTask and FetchTasks components to the API client

--- Front-end/admin-web-portal/src/Global/Scheduler/Scheduler.js
+++ Front-end/admin-web-portal/src/Global/Scheduler/Scheduler.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useState, useEffect } from "react";
 import Paper from "@mui/material/Paper";
 import {
 	ViewState,
@@ -19,51 +19,20 @@ import {
 	TodayButton,
 	CurrentTimeIndicator,
 } from "@devexpress/dx-react-scheduler-material-ui";
-import { styled } from "@mui/material/styles";
 import Grid from "@mui/material/Grid";
-import TextField from "@mui/material/TextField";
+
+// React Query
+import { useQuery } from "@tanstack/react-query";
 
 // API
 import AddTask from "../../api-client/Scheduler/AddTask";
-
-const appointments = [
-	{
-		id: 0,
-		title: "Watercolor Landscape",
-		startDate: new Date(2017, 4, 28, 9, 30),
-		endDate: new Date(2017, 4, 28, 12, 0),
-	},
-	{
-		id: 1,
-		title: "Oil Painting for Beginners",
-		startDate: new Date(2017, 4, 28, 12, 30),
-		endDate: new Date(2017, 4, 28, 14, 30),
-	},
-	{
-		id: 2,
-		title: "Testing",
-		startDate: new Date(2017, 4, 29, 12, 30),
-		endDate: new Date(2017, 4, 29, 14, 30),
-	},
-	{
-		id: 3,
-		title: "Final exams",
-		startDate: new Date(2017, 4, 29, 9, 30),
-		endDate: new Date(2017, 4, 29, 12, 0),
-	},
-];
+import FetchTasks from "../../api-client/Scheduler/FetchTasks";
+import EditTask from "../../api-client/Scheduler/EditTask";
 
 const date = new Date();
 const current = `${date.getFullYear()}-${
 	date.getMonth() + 1
 }-${date.getDate()}`;
-
-const PREFIX = "Demo";
-
-const classes = {
-	checkBoxContainer: `${PREFIX}-checkBoxContainer`,
-	textField: `${PREFIX}-textField`,
-};
 
 const formattedDate = (date) => {
 	const parsedDate = new Date(date);
@@ -80,100 +49,95 @@ const formattedDate = (date) => {
 	return formattedDate;
 };
 
-export default class Demo extends React.PureComponent {
-	constructor(props) {
-		super(props);
-		this.state = {
-			data: appointments,
-			shadePreviousCells: true,
-			shadePreviousAppointments: true,
-			updateInterval: 10000,
-		};
+const Calendar = () => {
+	const [appointments, setAppointments] = useState([]);
+	const [shadePreviousCells, setShadePreviousCells] = useState(true);
+	const [shadePreviousAppointments, setShadePreviousAppointments] =
+		useState(true);
+	const [updateInterval, setUpdateInterval] = useState(10000);
 
-		this.commitChanges = this.commitChanges.bind(this);
+	const {
+		status,
+		error,
+		data: tasksData,
+	} = useQuery(["tasks_data"], FetchTasks);
+	useEffect(() => {
+		if (tasksData) {
+			if (tasksData.length > 0) {
+				setAppointments(tasksData);
+			} else {
+				alert("No staff found");
+			}
+		}
+	}, [tasksData, status]);
 
-		this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
-		this.handleUpdateIntervalChange = (nextValue) => {
-			this.setState({
-				updateInterval: nextValue * 1000,
+	const commitChanges = ({ added, changed, deleted }) => {
+		let data = [...appointments];
+		if (added) {
+			added.startDate = formattedDate(added.startDate);
+			added.endDate = formattedDate(added.endDate);
+			const response = AddTask(added);
+			response.then((res) => {
+				if (res.message !== "successful") {
+					alert("Something went wrong");
+				}
 			});
-		};
-	}
+			const startingAddedId =
+				data.length > 0 ? data[data.length - 1].id + 1 : 0;
+			data = [...data, { id: startingAddedId, ...added }];
+		}
+		if (changed) {
+			const taskid = Object.keys(changed)[0];
+			const response = edit(changed[taskid], taskid);
+			response.then((res) => {
+				if (res.message !== "task editted successfuly") {
+					alert("Something went wrong");
+				}
+			});
 
-	handleCheckboxChange(stateField) {
-		const { [stateField]: fieldToChange } = this.state;
-		this.setState({
-			[stateField]: !fieldToChange,
-		});
-	}
+			data = data.map((appointment) =>
+				changed[appointment.id]
+					? { ...appointment, ...changed[appointment.id] }
+					: appointment,
+			);
+		}
+		if (deleted !== undefined) {
+			data = data.filter((appointment) => appointment.id !== deleted);
+		}
+		setAppointments(data);
+	};
 
-	commitChanges({ added, changed, deleted }) {
-		this.setState((state) => {
-			let { data } = state;
-			if (added) {
-				added.startDate = formattedDate(added.startDate);
-				added.endDate = formattedDate(added.endDate);
-				console.log(added);
-				const response = AddTask(added);
-				response.then((res) => {
-					console.log(res);
-				});
-				const startingAddedId =
-					data.length > 0 ? data[data.length - 1].id + 1 : 0;
-				data = [...data, { id: startingAddedId, ...added }];
-			}
-			if (changed) {
-				data = data.map((appointment) =>
-					changed[appointment.id]
-						? { ...appointment, ...changed[appointment.id] }
-						: appointment,
-				);
-			}
-			if (deleted !== undefined) {
-				data = data.filter((appointment) => appointment.id !== deleted);
-			}
-			return { data };
-		});
-	}
+	return (
+		<React.Fragment>
+			<Grid container paddingTop='1em' paddingLeft='1em'>
+				<h2>Staff Schedule</h2>
+			</Grid>
+			<Paper>
+				<Scheduler data={appointments} height={850}>
+					<ViewState defaultCurrentDate={current} />
+					<EditingState onCommitChanges={commitChanges} />
 
-	render() {
-		const {
-			data,
-			shadePreviousCells,
-			updateInterval,
-			shadePreviousAppointments,
-		} = this.state;
+					<WeekView />
+					<DayView />
+					<Appointments />
+					<IntegratedEditing />
 
-		return (
-			<React.Fragment>
-				<Grid container paddingTop='1em' paddingLeft='1em'>
-					<h2>Staff Schedule</h2>
-				</Grid>
-				<Paper>
-					<Scheduler data={data} height={850}>
-						<ViewState defaultCurrentDate={current} />
-						<EditingState onCommitChanges={this.commitChanges} />
+					<Toolbar />
+					<ViewSwitcher />
+					<DateNavigator />
+					<TodayButton />
+					<AppointmentTooltip showOpenButton />
+					<AppointmentForm />
+					<DragDropProvider />
+					<CurrentTimeIndicator
+						shadePreviousCells={shadePreviousCells}
+						shadePreviousAppointments={shadePreviousAppointments}
+						updateInterval={updateInterval}
+					/>
+				</Scheduler>
+			</Paper>
+		</React.Fragment>
+	);
+};
 
-						<WeekView />
-						<DayView />
-						<Appointments />
-						<IntegratedEditing />
-
-						<Toolbar />
-						<ViewSwitcher />
-						<DateNavigator />
-						<TodayButton />
-						<AppointmentTooltip showOpenButton />
-						<AppointmentForm />
-						<DragDropProvider />
-						<CurrentTimeIndicator
-							shadePreviousCells={shadePreviousCells}
-							shadePreviousAppointments={shadePreviousAppointments}
-							updateInterval={updateInterval}
-						/>
-					</Scheduler>
-				</Paper>
-			</React.Fragment>
-		);
-	}
-}
+export default Calendar;

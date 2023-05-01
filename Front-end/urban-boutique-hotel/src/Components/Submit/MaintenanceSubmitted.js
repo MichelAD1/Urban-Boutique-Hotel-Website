c278d079d30edb5b16fbe6be3a2cdd1baf6ca072
MichelAD1 <michel.a.abidaoud@gmail.com>
2023-05-01 15:44:31 +0300

    Creating maintenance design page

--- Front-end/urban-boutique-hotel/src/Components/Submit/MaintenanceSubmitted.js
+++ Front-end/urban-boutique-hotel/src/Components/Submit/MaintenanceSubmitted.js
@@ -1,26 +1,62 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 
 const MaintenanceSubmitted = () => {
   const navigate = useNavigate();
   const { t, i18n } = useTranslation();
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const timeoutId = setTimeout(() => {
-      navigate("/");
-    }, 5000);
+      setIsLoading(false);
+    }, 3000);
 
     return () => clearTimeout(timeoutId);
-  }, [navigate]);
+  }, []);
+
   useEffect(() => {
     i18n.changeLanguage(localStorage.getItem("Translate"));
   }, []);
 
+  useEffect(() => {
+    if (!isLoading) {
+      const timeoutId = setTimeout(() => {
+        navigate("/account");
+      }, 2000);
+      return () => clearTimeout(timeoutId);
+    }
+  }, [isLoading, navigate]);
+
   return (
     <div className="faq-container">
-      <h1>Submitting Request</h1>
-      <div className="buffer-loader submit"></div>
+      {isLoading ? (
+        <>
+          <h1>Submitting Maintenance Request</h1>
+          <div className="buffer-loader submit"></div>
+        </>
+      ) : (
+        <>
+          <h1>Maintenance Request Submitted</h1>
+          <div className="checkmark-container">
+            <div className="check-mark-container">
+              <svg className="check-mark-svg" viewBox="0 0 52 52">
+                <circle
+                  className="check-mark-circle"
+                  cx="26"
+                  cy="26"
+                  r="25"
+                  fill="none"
+                />
+                <path
+                  className="check-mark-path"
+                  d="M14.1 27.2l7.1 7.2 16.7-16.8"
+                />
+              </svg>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
